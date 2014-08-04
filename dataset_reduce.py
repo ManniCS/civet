c97eea041db08c3784a2942ac839cd90250d4594
ManniCS <mcsforza@stanford.edu>
2014-08-04 16:41:51 -0700

    Adding scripts written over the weekend. Now collapsing nodes recursively; not yet expanding them recursively. Files for test input, and a note on generating diffed files with perforce also included.

---
+++ dataset_reduce.py
@@ -0,0 +1,95 @@
+"""Understanding this file will be aided by understanding the dot_to_json.py 
+   file, the output of which this file operates on. """ 
+
+import json
+import sys
+
+"""Grab input and ouput files from the command line"""
+input_file, output_file = "", ""
+
+if len(sys.argv) < 3: 
+	print "Please enter a diffed file, an input file, and an output file, ending in a .txt, .json and ._json extension, respectively"
+	sys.exit()
+
+found_input_file = False
+found_output_file = False
+
+for i in xrange(1, len(sys.argv)):
+	f = sys.argv[i]
+	if '.txt' in f: 
+		diffed_file = f
+	if '.json' in f: 
+		found_input_file = True
+		input_file = f
+	if '._json' in f: 
+		found_output_file = True
+		output_file = f
+
+if not (found_output_file and found_input_file): 
+	print "Please enter a diffed file, an input file, and an output file, ending in a .txt, .json and ._json extension, respectively"
+	sys.exit()
+
+"""Load json object into a python variable."""
+dataset = json.load(open(input_file, "r"))
+diffed_file = open(diffed_file, "r")
+output_file = open(output_file, "w")
+
+"""Set up necessary script-wide variables"""
+index = 0
+child_index = 0
+diffed_files = []
+discovered_nodes = {} # dictionary of discovered nodes: these have already been given,
+				  	  # and are stored with, their unique index (the values of the dict)
+nodes = dataset["nodes"] #array of JSON objects = loaded nodes
+links = dataset["links"] #array of JSON objects = loaded edges
+
+reduced_nodes = []#array of JSON objects representing the reduced set of nodes necessary 
+				  #for initializing the force-directed layout in test.html
+reduced_links = []#array of JSON objects representing the reduced set of links necessary 
+				  #for initializing the force-directed layout in test.html
+child_nodes = [] #the children of the loaded nodes
+
+"""Setup helper functions"""
+def childWithName(name):
+	for elem in nodes: 
+		if elem["name"] == name: return elem
+
+"""Grab list of diffed files from the diffed file"""
+next_line = "!"
+while next_line != "":
+	next_line = diffed_file.readline()
+	next_line = next_line.strip()
+	next_line = next_line[0:len(next_line) - 2]
+	if next_line:
+		diffed_files.append(next_line)
+
+"""Add the JSON objects corresponding to the files in the list of diffed files
+   to the reduced collection of nodes."""
+for elem in nodes: 
+	if elem["name"] in diffed_files:
+		discovered_nodes[elem["name"]] = index
+		reduced_nodes.append(elem)
+		index += 1
+
+print reduced_nodes
+
+"""Add the files dependent on the diffed files (their children) to the collection
+   of reduced nodes, and their links to the set of reduced links."""
+for elem in reduced_nodes: 
+	print elem["name"]
+	parent_index = discovered_nodes[elem["name"]]
+	for child in elem["children"]:
+		if child in discovered_nodes:
+			child_indx = discovered_nodes[child]
+			reduced_links.append({"source": parent_index, "target": child_indx})
+		else: 
+			child_indx = len(reduced_nodes) + child_index #zero indexed (bc child index starts at 0)
+			discovered_nodes[child] = child_indx
+			child_nodes.append(childWithName(child))
+			reduced_links.append({"source": parent_index, "target": child_indx})
+			child_index += 1
+
+"""Assemble and write the final object to a file"""
+final_object = {"nodes": reduced_nodes + child_nodes, "links": reduced_links}
+
+output_file.write(json.dumps(final_object))
---
+++ dot_to_json.py
@@ -0,0 +1,128 @@
+""" This file parses a dot file and writes the parsed content in 
+	json format for graphs, accepted by the d3 force-directed 
+	layout. This file assumes that the nodes related by the dot 
+	file are filenames that do not contain spaces, and ignores 
+	all edge color, and other formatting details, that can be 
+	specified in a dot file. 
+"""
+import json
+import sys
+
+input_file, output_file = "", ""
+
+"""Grab input and ouput files from the command line"""
+if len(sys.argv) < 2: 
+	print "Please enter both an input and an output file, ending in a .dot and .json extension, respectively"
+	sys.exit()
+
+found_input_file = False
+found_output_file = False
+
+for i in xrange(1, len(sys.argv)):
+	print i
+	f = sys.argv[i]
+	print f
+	if '.dot' in f: 
+		found_input_file = True
+		input_file = f
+	if '.json' in f: 
+		found_output_file = True
+		output_file = f
+
+if not (found_output_file and found_input_file): 
+	print "Please enter both an input and an output file, ending in a .dot and .json extension, respectively"
+	sys.exit()
+
+"""Open files"""
+f = open(input_file, "r")
+o = open(output_file, "w")
+
+"""Set up necessary script-wide variables"""
+index = 0
+discovered_nodes = {} # dictionary of discovered nodes: these have already been given,
+				  	  # and are stored with, their unique index (the values of the dict)
+nodes = [] #array of JSON objects representing nodes
+links = [] #array of JSON objects representing edges
+
+"""
+Recursive method that allows for parsing of lines in the dot 
+file that specify relationships between multiple nodes. 
+"""
+def addNodesAndLinks(tokens):
+	if len(tokens) == 1: #if there is only a child left 
+		return     		 #return 
+
+	"""Bind the appropriate variables to their global equivalents"""
+	global index
+
+	"""Add nodes to collection as child and parent"""
+	parent = tokens[0]
+	child = tokens[1]
+
+	if (child == "{}"): return #a possible way of encoding no children (obviously, 
+							   #we don't want this to be included as a child node though)
+	"""Add child and parent into collection of discovered nodes, 
+	   if not yet discovered and give a unique index"""
+	if not parent in discovered_nodes:
+		discovered_nodes[parent] = index
+		index += 1
+	if not child in discovered_nodes:
+		discovered_nodes[child] = index
+		index += 1
+
+	"""Check if parent and child exist. If they do, simply add child to parent"""
+	child_exists = False
+	parent_exists = False
+	for i, elem in enumerate(nodes):
+		if elem["name"] == parent:
+			parent_exists = True
+			nodes[i]["children"].append(child)
+		elif elem["name"] == child:
+			child_exists = True
+
+	"""If child or parent do not exist, add the missing ones to the nodes list. 
+	   Be sure to push these in this order. This ensures that the node list stays 
+	   sorted according to id.
+
+	   Note: Because a node is never added to the list of nodes before it is assigned 
+	   the next available unique id (and then inserted into the list of discovered nodes)
+	   the ids of the nodes correspond to their index in the final dataset.)"""
+	if not parent_exists:
+		print "Adding parent + child!"
+		nodes.append({"name": parent, "children": [child]}) 
+	if not child_exists:
+		print "Adding child!"
+		nodes.append({"name": child, "children": []})
+
+	"""Add the parent-child relationship to the links list. The discovered nodes 
+       dictionary stores information about the ids of the nodes."""
+	links.append({"source": discovered_nodes[parent], "target": discovered_nodes[child]})
+
+	"""Recursively call the method on the rest of the tokens"""
+	addNodesAndLinks(tokens[1:len(tokens)])
+
+"""Parse file"""                                                  
+next_line = "!"
+while next_line != "":
+	next_line = f.readline()
+	"""If line does not contain edge information, skip"""
+	if not '->' in next_line:
+		continue;
+
+	"""Format string for splitting"""
+	next_line = next_line.replace(" ", "") #remove white space
+	next_line = next_line.replace('"', "") #remove quote-marks
+	next_line = next_line.strip() #remove trailing,leading whitespace
+	next_line = next_line.replace(";", "") #remove semicolon
+
+	"""Split string"""
+	tokens = next_line.split("->") #split line on arrows
+
+	"""Recursively adds nodes and links from a coded link information
+	to the respective datastructures"""
+	addNodesAndLinks(tokens)
+
+object = {"nodes": nodes, "links": links}
+
+"""Write to file"""
+o.write(json.dumps(object))
